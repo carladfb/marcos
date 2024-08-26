2d22d214bb930db5dcdf4b82cc3197819704b3a4
Guilherme <domiuaunoob@outlook.com>
2024-08-26 15:26:08 -0300

    charts

--- marcos/Views/ChartsView.swift
+++ marcos/Views/ChartsView.swift
@@ -6,12 +6,63 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ChartsView: View {
+    
+    @State var vetor: [GraficoTeste] = [
+        GraficoTeste(algo: "coisa1" , porcentagem: 10),
+        GraficoTeste(algo: "coisa2" , porcentagem: 13),
+        GraficoTeste(algo: "coisa3" , porcentagem: 16),
+        GraficoTeste(algo: "coisa4" , porcentagem: 18),
+        GraficoTeste(algo: "coisa5" , porcentagem: 5),
+        GraficoTeste(algo: "coisa6" , porcentagem: 5),
+        GraficoTeste(algo: "coisa7" , porcentagem: 15),
+        GraficoTeste(algo: "coisa8" , porcentagem: 5),
+        GraficoTeste(algo: "coisa9" , porcentagem: 5),
+        GraficoTeste(algo: "coisa10" , porcentagem: 5),
+        GraficoTeste(algo: "coisa11" , porcentagem: 5),
+        GraficoTeste(algo: "coisa12" , porcentagem: 25)
+        
+    ];
+    
     var body: some View {
-        Text("graficos")
+        GeometryReader {
+            geometry in
+            VStack {
+                ScrollView(.horizontal) {
+                    HStack {
+                        Chart {
+                            ForEach (vetor) {
+                                dado in
+                                BarMark(x: .value("coisa1", dado.algo),
+                                        y: .value("coisa2", dado.porcentagem))
+                                .annotation {
+                                    Text(String(Int(dado.porcentagem))).font(.caption2)
+                                }
+                            }
+                            
+                            
+                        }
+                        .padding(.vertical)
+                        .foregroundColor(.pink)
+                        
+                    }.frame(width: geometry.size.width * 2, height: 300)
+                    
+                }
+                
+                
+            }
+        }
     }
+    
 }
+    
+    struct GraficoTeste: Identifiable {
+        let id = UUID();
+        let algo: String;
+        let porcentagem: Double;
+    }
 
 #Preview {
     ChartsView()
