c823828ddcfba876c370901ca9d3ea81f2a27d02
carladfb <carladheyslane2@gmail.com>
2024-08-26 15:01:10 -0300

    Initial Commit

---
+++ marcos/marcosApp.swift
@@ -0,0 +1,17 @@
+//
+//  marcosApp.swift
+//  marcos
+//
+//  Created by CARLA DHEYSLANE FERREIRA BRITO on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct marcosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
