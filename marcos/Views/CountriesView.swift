e5c55400173b8795b2ae1e8579b565bb16ceb570
Noá <noivtroivsky@gmail.com>
2024-08-26 16:26:16 -0300

    eu tenho burro assinado NOá

--- marcos/Views/CountriesView.swift
+++ marcos/Views/CountriesView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct CountriesView: View {
+    
+    @State var countryName:String = ""
+    
     var body: some View {
-        Text("selecionar paises")
+        
+        NavigationStack{
+            List {
+                LazyVStack {
+                    ForEach(1...10, id: \.self) { count in
+                        
+                        Button(action: {
+                            print(count)
+                        }, label: {
+                            RoundedRectangle(cornerRadius: 15)
+                                .overlay {
+                                    Text("botao numero " + String(count)).foregroundColor(.white)
+                                }
+                                .frame(width: 330, height: 75)
+                                
+                                
+                        })
+                        
+                   
+                    }
+                }           
+            }
+        }.searchable(text: $countryName)
     }
 }
 
