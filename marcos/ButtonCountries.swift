31b231ce31ab3b2b270b6ac0dd2d3c63dafd5bae
carladfb <carladheyslane2@gmail.com>
2024-08-28 16:06:47 -0300

    home faltando coisa

---
+++ marcos/ButtonCountries.swift
@@ -0,0 +1,39 @@
+//
+//  ButtonCountries.swift
+//  marcos
+//
+//  Created by CARLA DHEYSLANE FERREIRA BRITO on 28/08/24.
+//
+
+import SwiftUI
+
+struct ButtonCountries: View {
+    @State private var isSheetPresented = false
+    @ObservedObject var holidaysDAO: HolidaysDAO
+    
+    var body: some View {
+        Button(action: {
+            isSheetPresented.toggle()
+        }) {
+            HStack(spacing: 4) {
+                Text("🇧🇷")
+                    .frame(width: 20, height: 20)
+                Text("BR")
+                    .foregroundColor(.black)
+                Image(systemName: "chevron.down")
+                    .foregroundColor(.vermeiTchan)
+            }
+            .padding(.vertical, 5)
+            .padding(.horizontal, 15)
+            .background(Color.white)
+            .cornerRadius(10)
+        }
+        .sheet(isPresented: $isSheetPresented) {
+            CountriesView(holidaysDAO: holidaysDAO)
+        }
+    }
+}
+
+#Preview {
+    ButtonCountries(holidaysDAO: HolidaysDAO())
+}
