9fe4c04c451c4e48b397410e9a7a015ff70ef9b7
carladfb <carladheyslane2@gmail.com>
2024-08-30 16:11:57 -0300

    ta

---
+++ marcos/Views/DashedLine.swift
@@ -0,0 +1,26 @@
+//
+//  DashedLine.swift
+//  marcos
+//
+//  Created by CARLA DHEYSLANE FERREIRA BRITO on 30/08/24.
+//
+
+import SwiftUI
+
+struct DashedLine: View {
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                let width = geometry.size.width
+                path.move(to: CGPoint(x: 0, y: 0))
+                path.addLine(to: CGPoint(x: width, y: 0))
+            }
+            .stroke(style: StrokeStyle(lineWidth: 1, dash: [5, 3]))
+            .foregroundColor(.vermeiTchan)
+        }
+    }
+}
+
+#Preview {
+    DashedLine()
+}
---
+++ marcos/Views/SheetHolidaysView.swift
@@ -0,0 +1,74 @@
+//
+//  SheetHolidaysView.swift
+//  marcos
+//
+//  Created by CARLA DHEYSLANE FERREIRA BRITO on 30/08/24.
+//
+
+import SwiftUI
+
+struct SheetHolidaysView: View {
+    @StateObject private var holidaysDAO = HolidaysDAO()
+    @State private var rotation: Double = 0
+    
+    var body: some View {
+        VStack {
+            if let actualHoliday = holidaysDAO.actualHoliday {
+                VStack(alignment: .leading, spacing: 12) {
+                    HStack {
+                        Text("Today's Holiday")
+                            .font(.title3)
+                            .padding(.bottom, 5)
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            withAnimation(.bouncy(duration: 0.5)) {
+                                rotation += 360
+                                holidaysDAO.fetchDays(currentCountry: holidaysDAO.actualCountry?.iso3166 ?? "BR") // Recarregar feriados
+                            }
+                        }, label: {
+                            Image(systemName: "arrow.clockwise")
+                                .font(.title2)
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 24, height: 24)
+                                .rotationEffect(.degrees(rotation))
+                        })
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                    
+                    DashedLine()
+                        .frame(height: 1)
+                        .padding(.vertical, 5)
+                    
+                    ScrollView {
+                        Text(actualHoliday.holiday.name)
+                            .font(.headline)
+                            .padding(.bottom, 5)
+                        
+                        Text(actualHoliday.holiday.description)
+                            .font(.body)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .frame(maxHeight: 150)
+                    
+                }
+                .padding()
+                .background(Color.white)
+                .cornerRadius(20)
+            } else {
+                Text("No holiday today")
+                    .padding()
+            }
+        }
+        .onAppear {
+            holidaysDAO.fetchDays(currentCountry: holidaysDAO.actualCountry?.iso3166 ?? "BR")
+        }
+        .padding(.horizontal, 30)
+        .padding(.vertical, 15)
+    }
+}
+
+#Preview {
+    SheetHolidaysView()
+}
